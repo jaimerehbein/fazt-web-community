45a167fb48817820741962531552d216b6738180
Willishakespeare <Skr13@outlook.com>
2020-07-10 16:52:06 -0700

    feature(fonts,buttons): added buttons and fonts

---
+++ src/components/Buttons/Buttons.jsx
@@ -0,0 +1,19 @@
+/* Copyright 2020 Fazt Community ~ All rights reserved. MIT license. */
+import React from 'react';
+import './Buttons.scss';
+
+export const ButtonSolid = ({text}) => {
+  return (
+    <div className="ComponentBTNSolidClass">
+      <span>{text}</span>
+    </div>
+  );
+};
+
+export const ButtonUnline = ({text}) => {
+  return (
+    <div className="ComponentBTNUnlineClass">
+      <span>{text}</span>
+    </div>
+  );
+};
