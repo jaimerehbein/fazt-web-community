0238b76dbc8e6e501fa54eef0859c2c033b5cc9f
Adrian Garcia Saaib <garciasaaib@gmail.com>
2020-07-11 13:27:34 -0600

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import './fonts/fonts.scss'
+import './fonts/fonts.scss';
 import Navigation from './components/Navigation/Navigation.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
   return (
     <div>
         <Navigation/>
+        <Footer/>
     </div>
   );
 }
