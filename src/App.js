95a9889e49ce69ae243e0546268634d9fbc0ad87
Alexsans <eddalexcab@gmail.com>
2020-07-11 10:29:07 -0600

    created(component): Social Links and edit Buttons.scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import './fonts/fonts.scss'
+import './fonts/fonts.scss';
 import Navigation from './components/Navigation/Navigation.jsx';
+import SocialLink from './components/SocialLink/SocialLink.jsx';
 
 function App() {
   return (
     <div>
         <Navigation/>
+        <SocialLink/>
     </div>
   );
 }
---
+++ src/components/SocialLink/SocialLink.jsx
@@ -0,0 +1,28 @@
+/* Copyright 2020 Fazt Community ~ All rights reserved. MIT license. */
+
+import React from 'react';
+import { ButtonSolid, ButtonUnline } from '../Buttons/Buttons.jsx';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import DiscordSvg from '../../img/discord-brands.svg';
+import './SocialLink.scss';
+
+const SocialLink = () => {
+     return (
+          <div className="socialLink">
+               <div><span className="socialLink__span">Únete</span> a nuestro Discord y <span className="socialLink__span">visita</span> nuestra organización en Github</div>
+               <p className="socialLink__p">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate consectetur laudantium voluptas illo unde.</p>
+               <div className="socialLink__links">
+                    <div className="socialLink__link">
+                         <img src={ DiscordSvg } alt="discord" />
+                         <ButtonSolid text="ÚNETE"/>
+                    </div>
+                    <div className="socialLink__link">
+                         <GitHubIcon fontSize="inherit"/>
+                         <ButtonUnline text="VISÍTANOS"/>
+                    </div>
+               </div>
+          </div>
+     );
+};
+
+export default SocialLink;
