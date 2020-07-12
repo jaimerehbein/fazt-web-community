9875e7297bc158140b4685f36c52bfc578028669
Willishakespeare <Skr13@outlook.com>
2020-07-12 02:44:26 -0700

    feature(fix): fix Doc and Add propsType

--- src/components/CardProyect/CardProyect.jsx
+++ src/components/CardProyect/CardProyect.jsx
@@ -1,41 +1,56 @@
 /* Copyright 2020 Fazt Community ~ All rights reserved. MIT license. */
-/* 
-Structure to use
-<CardProyect 
-        nameProyect="String!" 
-        imageUrl="String"
-        arrayProyects=
-        [
-            {
-                name:"String!",
-                url:"String!",
-                color:"String!"
-            }
-        ]
-        favAmount=Int!
-        />
-*/
+
+/**
+ * <Component for showing details of the proyect>
+ * @component
+ * @param   {String}    nameProyect     Name of Proyect
+ * @param   {String}    imageUrl        Url Of Image Proyect
+ * @param   {Array}     arrayProyects   Array of Technologies
+ * @param   {Number}    favAmount       Amount of Favorites
+ * @return  {<CardProyect>}
+ * @example
+ * const nameProyect= "React App Name"
+ * const imageUrl = "https://api/image.jpg"
+ * const arrayProyects = [
+ *                         {
+ *                            name: "React",
+ *                            url: "/img/icons/react.svg",
+ *                            color: "#FFFFFF"
+ *                         },
+ *                         {
+ *                            name: "GraphQL",
+ *                            url: "/img/icons/graphql.svg",
+ *                            color: "#FFFFFF"
+ *                         },
+ *                         {
+ *                            name: "Python",
+ *                            url: "img/icons/python.svg"
+ *                            color: "#FFFFFF"
+ *                         }]
+ * const favAmount = 10
+ * returns(
+ * <CardProyect nameProyect={nameProyect} imageUrl={imageUrl} arrayProyects={arrayProyects} favAmount={favAmount} >
+ * )
+ */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import './CardProyect.scss';
 import heartIcon from '../../img/icons/heart.svg';
 import { usePalette } from 'react-palette';
 
-export default function CardProyect({ imageUrl, nameProyect, arrayProyects, favAmount }) {
+const CardProyect = ({nameProyect, imageUrl, arrayProyects, favAmount }) => {
   var { data } = usePalette(imageUrl);
   data = data.vibrant;
-
   if (!imageUrl) {
     data = arrayProyects[0].color;
     imageUrl = process.env.PUBLIC_URL + arrayProyects[0].url;
   }
-
   var backgroundStyle = {
     backgroundImage: `url(${imageUrl})`,
     backgroundPosition: 'center center',
     backgroundSize: 'cover',
   };
-
   return (
     <div className="proyectCard">
       <div className="proyectCard__background" style={backgroundStyle}>
@@ -46,7 +61,7 @@ export default function CardProyect({ imageUrl, nameProyect, arrayProyects, favA
             </div>
             <div className="proyectCard__container__upSide__containerBar">
               <div className="proyectCard__container__upSide__containerBar__icons">
-                {arrayProyects.map((item, i) => (
+                {arrayProyects.slice(0, 3).map((item, i) => (
                   /*for now the items do not redirect missing add the routes*/
                   <img
                     key={i}
@@ -89,4 +104,32 @@ export default function CardProyect({ imageUrl, nameProyect, arrayProyects, favA
       </div>
     </div>
   );
-}
+};
+CardProyect.propTypes = {
+  /**
+   * Name of Proyect nameProyect
+   */
+  nameProyect: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string,
+  arrayProyects: [
+    {
+      name: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired,
+    },
+  ],
+  favAmount: PropTypes.number,
+};
+CardProyect.defaultProps = {
+  nameProyect: 'nameProyect',
+  imageUrl: '',
+  arrayProyects: [
+    {
+      name: 'React',
+      url: '/img/icons/react.svg',
+      color: '#53C1DE',
+    },
+  ],
+  favAmount: '0',
+};
+export default CardProyect;
