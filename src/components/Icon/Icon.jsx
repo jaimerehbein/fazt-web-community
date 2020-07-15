d339017c5f7bdcfb38c3484735fdcdfa8e141a93
Yuborama <davidjohan2@hotmail.com>
2020-07-15 00:35:18 -0500

    created(component): Icon

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,15 @@
+/* Copyright 2020 Fazt Community ~ All rights reserved. MIT license. */
+import React from 'react';
+import './Icon.scss';
+
+
+export const Icon = ({ src, icon: ComponentIcon, form }) => {
+  console.log('src'+src);
+  if (!ComponentIcon) {
+    console.log('hasta aqui llego');
+    return <img className={'icon ' + form} src={src} alt="icon" />;
+  } else {
+    console.log('else');
+    return <ComponentIcon className={'icon ' + form} fontSize="inherit"/>;
+  }
+};
